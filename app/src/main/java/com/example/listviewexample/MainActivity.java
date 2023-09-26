be732b23213ef969815596e740d887cec66f1a67
Maggie Harvey <mharvey82902@gamil.com>
2023-09-26 13:43:42 -0500

    added listener

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.listviewexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.LinkedList;
 
@@ -14,12 +17,20 @@ public class MainActivity extends AppCompatActivity {
 
     ListView petList;
 
+    AdapterView.OnItemClickListener listen = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            Toast.makeText(getApplicationContext(), petList.getItemAtPosition(i).toString(), Toast.LENGTH_LONG).show();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         petList = findViewById(R.id.petList);
+        petList.setOnItemClickListener(listen);
 
         int selection = 2;
         populateListView(selection);
